4922770b71b3d5ab3edc6fc03e5356a3546fc7ae
alabalag1 <alabalag1@gmail.com>
2022-06-12 18:49:13 +0300

    Updated line class

--- line.hpp
+++ line.hpp
@@ -8,23 +8,15 @@
 class Line : public Figure
 {
 private:
-    float x1;
-    float y1;
-    float x2;
-    float y2;
+    float m_x1;
+    float m_y1;
+    float m_x2;
+    float m_y2;
 
 public:
-    Line(/* args */);
+    Line() : m_x1{0}, m_y1{0}, m_x2{0}, m_y2{0} {};
+    Line(float x1, float y1, float x2, float y2) : m_x1{x1}, m_y1{y1}, m_x2{x2}, m_y2{y2} {};
     ~Line();
 };
-
-Line::Line(/* args */)
-{
-}
-
-Line::~Line()
-{
-}
-
 
 #endif
