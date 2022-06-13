9fc5758b4c4d76b316f185618671d3af06ff9bb9
alabalag1 <alabalag1@gmail.com>
2022-06-13 18:54:16 +0300

    Fixes

---  circle.cpp
+++
@@ -1,5 +0,0 @@
-#include "circle.hpp"
-#include<iostream>
-
-Circle::Circle(const char *type, float x, float y, float r, const char *fill, const char *stroke, float strokeWidth)
-    : Figure(type, fill, stroke, strokeWidth), m_x{x}, m_y{y}, m_r{r} {}
